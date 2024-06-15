bde5beed30736a931770f83bc0d38fdcd4c7f5eb
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-15 19:00:02 +0100

    auto-commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,7 @@ dotenv.config({path: "./config/.env"})
 
 const router = express.Router()
 router.get('/google',
-  passport.authenticate('google', { scope: ['profile', 'email'], prompt: 'select_account' })
+  passport.authenticate('google', { prompt: 'select_account' })
 );
 router.get("/google/callback",
   passport.authenticate("google", {

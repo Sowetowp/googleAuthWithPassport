34f01619a06ebc7d983002716e78889668d65b3d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-15 19:05:07 +0100

    auto-commit

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
 
     const handleGoogleAuth = () => {
         try {
-            window.location.href = ``
+            window.location.href = `http://localhost:5000/auth/google`
         } catch (err) {
             toast.error(err?.data?.message || err.error)
 
